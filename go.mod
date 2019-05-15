37fb3e58c883a2c59ad15fa6a8ba425000e54f07
Dennis Marttinen <dennis@weave.works>
2019-05-15 13:11:39 +0300

    Image building (from .tar.gz) implemented using mounting, native is WIP

--- go.mod
+++ go.mod
@@ -3,18 +3,19 @@ module github.com/luxas/ignite
 go 1.12
 
 require (
-	github.com/cloudflare/cfssl v0.0.0-20190510060611-9c027c93ba9e // indirect
 	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/cadvisor v0.33.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/lithammer/dedent v1.1.0
+	github.com/lunixbochs/struc v0.0.0-20190326164542-a9e4041416c2 // indirect
 	github.com/mholt/archiver v3.1.1+incompatible
+	github.com/nerd2/gexto v0.0.0-20180530205145-da6bfa566d33
 	github.com/nwaples/rardecode v1.0.0 // indirect
 	github.com/pierrec/lz4 v2.0.5+incompatible // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	gopkg.in/yaml.v2 v2.2.1 // indirect
 	sigs.k8s.io/yaml v1.1.0
--- pkg/image/image.go
+++ pkg/image/image.go
@@ -1,8 +1,16 @@
 package build
 
 import (
+	"archive/tar"
+	"io/ioutil"
+	"path/filepath"
+
+	//"archive/tar"
+	"fmt"
 	"github.com/luxas/ignite/pkg/constants"
 	"github.com/luxas/ignite/pkg/util"
+	"github.com/mholt/archiver"
+	"github.com/nerd2/gexto"
 	"github.com/pkg/errors"
 	"os"
 	"path"
@@ -31,8 +39,96 @@ func (i Image) AllocateAndFormat() error {
 		return errors.Wrapf(err, "failed to allocate space for image %s", i.id)
 	}
 
-	if _, err := util.ExecuteCommand("mkfs.ext4", i.path); err != nil {
+	// Use mkfs.ext4 to create the new image with an inode size of 128 (gexto doesn't support the default of 256)
+	if _, err := util.ExecuteCommand("mkfs.ext4", "-I", "128", "-E", "lazy_itable_init=0,lazy_journal_init=0", i.path); err != nil {
 		return errors.Wrapf(err, "failed to format image %s", i.id)
+	}
+
+	return nil
+}
+
+// Adds all the files from the given rootfs tar to the image
+// TODO: Fix the "corrupt direntry" error from gexto
+func (i Image) AddFiles(sourcePath string) error {
+	// TODO: This
+	filesystem, err := gexto.NewFileSystem(i.path)
+	if err != nil {
+		return err
+	}
+	//defer filesystem.Close()
+
+	if err := archiver.Walk(sourcePath, func(f archiver.File) error {
+		th, ok := f.Header.(*tar.Header)
+
+		if !ok {
+			return fmt.Errorf("expected header to be *tar.Header but was %T", f.Header)
+		}
+
+		//f.FileInfo
+		//data, err := os.Stat(f.Name())
+		//if err != nil {
+		//	return err
+		//}
+
+		relativePath, _ := filepath.Rel(".", th.Name)
+		filePath := filepath.Join("/", relativePath)
+
+		if f.IsDir() {
+			fmt.Printf("Directory: %s\n", filePath)
+			if err := filesystem.Mkdir(filePath, 0777); err != nil {
+				return fmt.Errorf("unable to create directory in filesystem: %s", filePath)
+			}
+		} else {
+			file, err := filesystem.Create(filePath)
+			if err != nil {
+				return fmt.Errorf("unable to create file in filesystem: %s", filePath)
+			}
+			//
+			//contents, err := ioutil.ReadAll(f)
+			//if err != nil {
+			//	return fmt.Errorf("unable to read file contents: %s", filePath)
+			//}
+			file.Write([]byte("Hello, world!"))
+			f.Close()
+		}
+
+		//if ok {
+		//	fmt.Println("Filename:", zfh.Name)
+		//}
+
+		//if f.IsDir() {
+		//	fmt.Println("Directory:", f.Name())
+		//} else {
+		//	fmt.Println("Filename:", f.Name())
+		//}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	filesystem.Close()
+
+	fmt.Println("Open success!")
+
+	return nil
+	// We need to open each file in the rootfs.tar and write it into fs
+}
+
+// mount-based file adder (temporary, requires root)
+func (i Image) AddFiles2(sourcePath string) error {
+	tempDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		return err
+	}
+	defer os.RemoveAll(tempDir)
+
+	if _, err := util.ExecuteCommand("mount", "-o", "loop", i.path, tempDir); err != nil {
+		return errors.Wrapf(err, "failed to mount image %s", i.id)
+	}
+	defer util.ExecuteCommand("umount", tempDir)
+
+	if err := archiver.Unarchive(sourcePath, tempDir); err != nil {
+		return err
 	}
 
 	return nil
