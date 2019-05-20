ec4470200b107f708bfbe640b44b87fa160f0c73
Dennis Marttinen <dennis@weave.works>
2019-05-20 13:40:30 +0300

    Add new MAC generator function and fix some typos

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -87,10 +87,10 @@ func NewID(baseDir string) (string, error) {
 	for {
 		idBytes = make([]byte, 8)
 		if _, err := rand.Read(idBytes); err != nil {
-			return "", errors.Wrap(err, "failed to generate ID")
+			return "", fmt.Errorf("failed to generate ID: %v", err)
 		}
 
-		// Convert the byte array to a string literally
+		// Convert the byte slice to a string literally
 		id = fmt.Sprintf("%x", idBytes)
 
 		// If the generated ID is unique break the generator loop
@@ -107,4 +107,45 @@ func NewID(baseDir string) (string, error) {
 
 	// Return the generated ID
 	return id, nil
+}
+
+// Fills the given string slice with unique MAC addresses
+func NewMAC(buffer *[]string) error {
+	var mac string
+	var macBytes []byte
+
+	for {
+		if len(*buffer) == cap(*buffer) {
+			break
+		}
+
+		macBytes = make([]byte, 6)
+		if _, err := rand.Read(macBytes); err != nil {
+			return fmt.Errorf("failed to generate MAC: %v", err)
+		}
+
+		// Set local bit, ensure unicast address
+		macBytes[0] = (macBytes[0] | 2) & 0xfe
+
+		// Convert the byte slice to a string literally
+		mac = fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", macBytes[0], macBytes[1], macBytes[2], macBytes[3], macBytes[4], macBytes[5])
+
+		// If the generated MAC is unique break the generator loop
+		unique := true
+		for _, testMac := range *buffer {
+			if mac == testMac {
+				unique = false
+				break
+			}
+		}
+
+		// Generate a new MAC if it's not unique
+		if !unique {
+			continue
+		}
+
+		*buffer = append(*buffer, mac)
+	}
+
+	return nil
 }
