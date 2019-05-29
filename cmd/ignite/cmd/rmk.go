7bdc7a0a144e079a6ad33fe53c3b402a627654d5
Dennis Marttinen <dennis@weave.works>
2019-05-29 15:05:19 +0300

    cmd refactor: restructure `ignite kernel`, fix `addimage` alias

--- cmd/ignite/cmd/rmk.go
+++ cmd/ignite/cmd/rmk.go
@@ -1,30 +1,14 @@
 package cmd
 
 import (
-	"github.com/luxas/ignite/cmd/ignite/run"
-	"github.com/luxas/ignite/pkg/errutils"
 	"github.com/spf13/cobra"
 	"io"
 )
 
-// NewCmdRmk removes the given kernel
+// NewCmdRmk is an alias for NewCmdKernelRm
 func NewCmdRmk(out io.Writer) *cobra.Command {
-	ro := &run.RmkOptions{}
-
-	cmd := &cobra.Command{
-		Use:   "rmk [id]",
-		Short: "Remove a kernel",
-		Args:  cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			errutils.Check(func() error {
-				var err error
-				if ro.Kernel, err = matchSingleKernel(args[0]); err != nil {
-					return err
-				}
-				return run.Rmk(ro)
-			}())
-		},
-	}
+	cmd := NewCmdKernelRm(out)
+	cmd.Use = "rmk [kernel]"
 
 	return cmd
 }
