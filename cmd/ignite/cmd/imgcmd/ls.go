aef21187d1bd5fa419f89c577cc2fa56df33e522
Dennis Marttinen <dennis.marttinen@gmail.com>
2019-06-06 18:54:11 +0300

    Add long descriptions to `image` and its subcommands.

--- cmd/ignite/cmd/imgcmd/ls.go
+++ cmd/ignite/cmd/imgcmd/ls.go
@@ -1,6 +1,7 @@
 package imgcmd
 
 import (
+	"github.com/lithammer/dedent"
 	"io"
 
 	"github.com/spf13/cobra"
@@ -9,9 +10,11 @@ import (
 // NewCmdLs lists available images
 func NewCmdLs(out io.Writer) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "ls",
-		Short:   "List available VM base images",
-		Long:    "TODO", // TODO: Long description
+		Use:   "ls",
+		Short: "List available VM base images",
+		Long: dedent.Dedent(`
+			List all available VM base images. Outputs the same as the parent command.
+		`),
 		Aliases: []string{"list"},
 		Run: func(cmd *cobra.Command, args []string) {
 			cmd.Parent().Run(cmd, args) // The parent command does this already, so just call it
