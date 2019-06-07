e9ce09b34db0fcf8ecac3198949d4d6925f4201e
Lucas Käldström <lucas@weave.works>
2019-06-07 10:49:54 +0100

    Add bash completion docs

--- cmd/ignite/cmd/completion.go
+++ cmd/ignite/cmd/completion.go
@@ -4,37 +4,28 @@ import (
 	"io"
 	"os"
 
+	"github.com/lithammer/dedent"
 	"github.com/spf13/cobra"
 	"github.com/weaveworks/ignite/pkg/errutils"
 )
-
-// completionCmd represents the completion command
-//var completionCmd = &cobra.Command{
-//	Use:   "completion",
-//	Short: "Generates bash completion scripts",
-//	Long: `To load completion run
-//
-//. <(bitbucket completion)
-//
-//To configure your bash shell to load completions for each session add to your bashrc
-//
-//# ~/.bashrc or ~/.profile
-//. <(bitbucket completion)
-//`,
-//	Run: func(cmd *cobra.Command, args []string) {
-//		rootCmd.GenBashCompletion(os.Stdout);
-//	},
-//}
 
 func NewCmdCompletion(out io.Writer, rootCmd *cobra.Command) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "completion",
 		Short: "Output bash completion for ignite to stdout",
+		Long: dedent.Dedent(`
+		In order to start using the auto-completion, run:
+
+			. <(ignite completion)
+		
+		To configure your bash shell to load completions for each session, run:
+
+			echo '. <(ignite completion)' >> ~/.bashrc
+		`),
 		Run: func(cmd *cobra.Command, args []string) {
 			err := rootCmd.GenBashCompletion(os.Stdout)
 			errutils.Check(err)
 		},
 	}
-
 	return cmd
 }
