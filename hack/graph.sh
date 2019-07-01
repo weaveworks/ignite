be7cc088c671c5728155fb146367a67d4ada4ea6
Lucas Käldström <lucas@weave.works>
2019-07-01 12:55:38 +0300

    add a graph of dependencies

---
+++ hack/graph.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
+cd ${SCRIPT_DIR}/..
+
+cat > graph.dot <<EOD
+digraph {
+	graph [overlap=false, size=14];
+	root="$(go list -m)";
+	node [  shape = plaintext, fontname = "Helvetica", fontsize=24];
+	"$(go list -m)" [style = filled, fillcolor = "#E94762"];
+    $(go mod graph | sed -Ee 's/@[^[:blank:]]+//g' | sort | uniq | awk '{print "\""$1"\" -> \""$2"\""};')
+}
+EOD
+
+sed -i 's+\("github.com/[^/]*/\)\([^"]*"\)+\1\\n\2+g' graph.dot
+sfdp -Tsvg -o docs/dependencies.svg graph.dot
+rm graph.dot
