be57768e969a4b5e0c81f47cf6e15e8232552add
Lucas Käldström <lucas@weave.works>
2019-07-02 23:53:29 +0300

    Add Pull and Inspect capabilities to the docker client

--- pkg/containerruntime/docker/client.go
+++ pkg/containerruntime/docker/client.go
@@ -3,6 +3,7 @@ package docker
 import (
 	"context"
 	"fmt"
+	"io"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -13,7 +14,7 @@ const dockerNetNSFmt = "/proc/%v/ns/net"
 
 // GetDockerClient builds a client for talking to docker
 func GetDockerClient() (containerruntime.Interface, error) {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithVersion("1.35"))
 	if err != nil {
 		return nil, err
 	}
@@ -28,6 +29,22 @@ type dockerClient struct {
 
 func (dc *dockerClient) RawClient() interface{} {
 	return dc.client
+}
+
+func (dc *dockerClient) InspectImage(image string) (*containerruntime.ImageInspectResult, error) {
+	res, _, err := dc.client.ImageInspectWithRaw(context.Background(), image)
+	if err != nil {
+		return nil, err
+	}
+	return &containerruntime.ImageInspectResult{
+		ID: res.ID,
+		Names: res.RepoTags,
+		Size: res.Size,
+	}, nil
+}
+
+func (dc *dockerClient) PullImage(image string) (io.ReadCloser, error) {
+	return dc.client.ImagePull(context.Background(), image, types.ImagePullOptions{})
 }
 
 // GetNetNS returns the network namespace of the given containerID. The ID
--- pkg/containerruntime/types.go
+++ pkg/containerruntime/types.go
@@ -1,6 +1,16 @@
 package containerruntime
 
+import "io"
+
+type ImageInspectResult struct {
+	ID string
+	Names []string
+	Size int64
+}
+
 type Interface interface {
+	InspectImage(image string) (*ImageInspectResult, error)
+	PullImage(image string) (io.ReadCloser, error)
 	GetNetNS(containerID string) (string, error)
 	RawClient() interface{}
 }
