65c91938e31342300e85ded3766c9e7dd8c23e2e
Lucas Käldström <lucas@weave.works>
2019-07-02 17:36:05 +0300

    omit the autogenerated timestamp in cobra command docs

--- hack/cobra.go
+++ hack/cobra.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra/doc"
 	"github.com/weaveworks/ignite/cmd/ignite/cmd"
@@ -13,4 +14,8 @@ func main() {
 	if err := doc.GenMarkdownTree(ignite, "./docs/cli"); err != nil {
 		log.Fatal(err)
 	}
+	if output, err := exec.Command("/bin/bash", "-c", `sed -e "/Auto generated/d" -i docs/cli/*.md`).CombinedOutput(); err != nil {
+		log.Fatal(string(output), err)
+	}
+	
 }
