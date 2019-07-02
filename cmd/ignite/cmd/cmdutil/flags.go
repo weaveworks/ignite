82bb1933c495d1194c700addcdf050379dd066a1
Lucas Käldström <lucas@weave.works>
2019-07-02 17:28:18 +0300

    Reflect earlier changes in the code in other places

--- cmd/ignite/cmd/cmdutil/flags.go
+++ cmd/ignite/cmd/cmdutil/flags.go
@@ -1,6 +1,9 @@
 package cmdutil
 
-import "github.com/spf13/pflag"
+import (
+	"github.com/spf13/pflag"
+	ignitemeta "github.com/weaveworks/ignite/pkg/apis/meta/v1alpha1"
+)
 
 // This file contains a collection of common flag adders
 func AddNameFlag(fs *pflag.FlagSet, name *string) {
@@ -17,4 +20,20 @@ func AddForceFlag(fs *pflag.FlagSet, force *bool) {
 
 func AddImportKernelFlags(fs *pflag.FlagSet, kernelName *string) {
 	fs.StringVarP(kernelName, "import-kernel", "k", *kernelName, "Import a new kernel from /boot/vmlinux in the image with the specified name")
+}
+
+func SizeVar(fs *pflag.FlagSet, ptr interface{}, flagName string, defaultSizeBytes int64, description string) {
+	SizeVarP(fs, ptr, flagName, "", defaultSizeBytes, description)
+}
+
+func SizeVarP(fs *pflag.FlagSet, ptr interface{}, flagName, shorthand string, defaultSizeBytes int64, description string) {
+	size := ignitemeta.NewSizeFromBytes(uint64(defaultSizeBytes))
+	switch v := ptr.(type) {
+	case *string:
+		fs.StringVarP(v, flagName, shorthand, size.String(), description)
+	case *int64:
+		fs.Int64VarP(v, flagName, shorthand, size.Int64(), description)
+	default:
+		panic("invalid size flag set up")
+	}
 }
