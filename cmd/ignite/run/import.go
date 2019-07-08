99502dff66f7b85fd90a1d6bb25ca5e7233e474f
Lucas Käldström <lucas@weave.works>
2019-07-08 16:57:25 +0300

    move all import logic into pkg/operations

--- cmd/ignite/run/import.go
+++ cmd/ignite/run/import.go
@@ -1,61 +1,27 @@
 package run
 
 import (
-	"fmt"
-
 	"github.com/weaveworks/ignite/pkg/client"
-	"github.com/weaveworks/ignite/pkg/filter"
 	"github.com/weaveworks/ignite/pkg/metadata"
+	"github.com/weaveworks/ignite/pkg/metadata/imgmd"
+	"github.com/weaveworks/ignite/pkg/metadata/kernmd"
 	"github.com/weaveworks/ignite/pkg/operations"
-	"github.com/weaveworks/ignite/pkg/storage/filterer"
 )
 
-type importOptions struct {
-	source string
-}
-
-func NewImportOptions(source string) (*importOptions, error) {
-	return &importOptions{source: source}, nil
-}
-
-func Import(bo *importOptions) error {
-	runImage, err := operations.ImportImage(bo.source)
+func ImportImage(source string) (*imgmd.Image, error) {
+	runImage, err := operations.FindOrImportImage(client.DefaultClient, source)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer metadata.Cleanup(runImage, false) // TODO: Handle silent
+	return runImage, metadata.Success(runImage)
+}
 
-	// If the kernel already exists, don't try to import something with the same name
-	if _, err := client.Kernels().Find(filter.NewNameFilter(runImage.GetName())); err == nil {
-		return metadata.Success(runImage)
-	} else {
-		fmt.Printf("err %T %v", err, err)
-		switch err.(type) {
-		case *filterer.AmbiguousError:
-			// such a kernel seem to already exist
-			return metadata.Success(runImage)
-		case *filterer.NonexistentError:
-			// If the kernel did not exist, let's import it
-			fmt.Println("kernel nonexistent, importing")
-		default:
-			// other, unknown error
-			return err
-		}
-	}
-	// at this point we know that there is no kernel with the same name as the image
-	// import a kernel from the image
-	runKernel, err := operations.ImportKernelFromImage(runImage)
+func ImportKernel(source string) (*kernmd.Kernel, error) {
+	runKernel, err := operations.FindOrImportKernel(client.DefaultClient, source)
 	if err != nil {
-		return err
-	}
-	if runKernel == nil {
-		// there was no kernel in the image, that's fine too
-		fmt.Println("no kernel in image")
-		return metadata.Success(runImage)
+		return nil, err
 	}
 	defer metadata.Cleanup(runKernel, false) // TODO: Handle silent
-
-	// both the image and kernel are imported successfully
-	metadata.Success(runKernel)
-	return metadata.Success(runImage)
+	return runKernel, metadata.Success(runKernel)
 }
