a5a4bb51462b941bc65b9bb992b51535e4cc314e
Dennis Marttinen <dennis@weave.works>
2019-07-15 19:58:53 +0300

    Support unquoted strings when unmarshaling Ignite UIDs from JSON

--- pkg/apis/meta/v1alpha1/uid.go
+++ pkg/apis/meta/v1alpha1/uid.go
@@ -1,6 +1,11 @@
 package v1alpha1
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/weaveworks/ignite/pkg/constants"
+	"strconv"
+	"unicode/utf8"
+)
 
 // UID represents an unique ID for a type
 type UID string
@@ -10,4 +15,25 @@ var _ fmt.Stringer = UID("")
 // String returns the UID in string representation
 func (u UID) String() string {
 	return string(u)
+}
+
+// This unmarshaler enables the UID to be passed in as an
+// unquoted string in JSON. Upon marshaling, quotes will
+// be automatically added.
+func (u *UID) UnmarshalJSON(b []byte) error {
+	if !utf8.Valid(b) {
+		return fmt.Errorf("invalid UID string: %s", b)
+	}
+
+	uid, err := strconv.Unquote(string(b))
+	if err != nil {
+		return err
+	}
+
+	if len(uid) < constants.IGNITE_UID_LENGTH {
+		return fmt.Errorf("UID string too short: %q", uid)
+	}
+
+	*u = UID(uid)
+	return nil
 }
