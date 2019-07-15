5b9bb3fe3949378dd6ad0d23421af87bd15774f1
Dennis Marttinen <dennis@weave.works>
2019-07-15 19:18:47 +0300

    Revert embedding a pointer, use metav1.Time.IsZero() in Time instead

--- pkg/apis/meta/v1alpha1/time.go
+++ pkg/apis/meta/v1alpha1/time.go
@@ -10,17 +10,16 @@ import (
 )
 
 type Time struct {
-	*metav1.Time
+	metav1.Time
 }
 
 var _ fmt.Stringer = Time{}
 
 var _ json.Marshaler = &Time{}
-var _ json.Unmarshaler = &Time{}
 
 // The default string for Time is a human readable difference between the Time and the current time
 func (t Time) String() string {
-	if t.Time == nil {
+	if t.IsZero() {
 		return "<unknown>"
 	}
 
@@ -30,47 +29,20 @@ func (t Time) String() string {
 // Timestamp returns the current UTC time
 func Timestamp() Time {
 	return Time{
-		&metav1.Time{
+		metav1.Time{
 			Time: time.Now().UTC(),
 		},
 	}
 }
 
-func newTime() Time {
-	return Time{
-		&metav1.Time{},
-	}
-}
-
-func (t Time) MarshalJSON() ([]byte, error) {
-	ti := t
-	if t.Time == nil {
-		// If the embedded metav1.Time is nil,
+func (t Time) MarshalJSON() (b []byte, err error) {
+	if t.Time.IsZero() {
+		// If the embedded metav1.Time is zero,
 		// use the current time when marshaling
-		ti = Timestamp()
+		b, err = Timestamp().Time.MarshalJSON()
+	} else {
+		b, err = t.Time.MarshalJSON()
 	}
 
-	b, err := ti.MarshalText()
-	if err != nil {
-		return nil, err
-	}
-
-	return json.Marshal(string(b))
-}
-
-func (t *Time) UnmarshalJSON(b []byte) error {
-	var str string
-
-	if err := json.Unmarshal(b, &str); err != nil {
-		return err
-	}
-
-	// If the Time is an empty string,
-	// it should not be loaded
-	if len(str) > 0 {
-		*t = newTime()
-		return t.UnmarshalText([]byte(str))
-	}
-
-	return nil
+	return
 }
