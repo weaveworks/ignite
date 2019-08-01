a551e6a5b2c411a03a449cafb9cb299812b693be
Lucas Käldström <lucas@weave.works>
2019-08-01 14:20:52 +0300

    generate ignite docs to docs/cli/ignite instead

--- hack/cobra.go
+++ hack/cobra.go
@@ -16,10 +16,10 @@ func main() {
 		log.Fatal(err)
 	}
 	ignite := cmd.NewIgniteCommand(os.Stdin, os.Stdout, os.Stderr)
-	if err := doc.GenMarkdownTree(ignite, "./docs/cli"); err != nil {
+	if err := doc.GenMarkdownTree(ignite, "./docs/cli/ignite"); err != nil {
 		log.Fatal(err)
 	}
-	if output, err := exec.Command("/bin/bash", "-c", `sed -e "/Auto generated/d" -i docs/cli/*.md`).CombinedOutput(); err != nil {
+	if output, err := exec.Command("/bin/bash", "-c", `sed -e "/Auto generated/d" -i docs/cli/ignite/*.md`).CombinedOutput(); err != nil {
 		log.Fatal(string(output), err)
 	}
 }
