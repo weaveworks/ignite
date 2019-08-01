fdeb33d5c5e860043bc9241666d8876f23c72f87
Lucas Käldström <lucas@weave.works>
2019-08-01 12:21:35 +0300

    Move MappedRawStorage to pkg/storage, as it's very generic

--- pkg/storage/manifest/storage.go
+++ pkg/storage/manifest/storage.go
@@ -4,14 +4,13 @@ import (
 	"github.com/weaveworks/ignite/pkg/apis/ignite/scheme"
 	"github.com/weaveworks/ignite/pkg/constants"
 	"github.com/weaveworks/ignite/pkg/storage"
-	"github.com/weaveworks/ignite/pkg/storage/manifest/raw"
 	"github.com/weaveworks/ignite/pkg/storage/sync"
 	"github.com/weaveworks/ignite/pkg/storage/watch"
 )
 
 func NewManifestStorage(dataDir string) (*ManifestStorage, error) {
 
-	ws, err := watch.NewGenericWatchStorage(storage.NewGenericStorage(raw.NewGenericMappedRawStorage(dataDir), scheme.Serializer))
+	ws, err := watch.NewGenericWatchStorage(storage.NewGenericStorage(storage.NewGenericMappedRawStorage(dataDir), scheme.Serializer))
 	if err != nil {
 		return nil, err
 	}
