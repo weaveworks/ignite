ed0e11031f91e779210693633829490a63d872e3
Dennis Marttinen <dennis@weave.works>
2019-08-23 21:16:28 +0300

    Implement containerd provider, disable it for now

--- pkg/providers/ignite/providers.go
+++ pkg/providers/ignite/providers.go
@@ -4,6 +4,8 @@ import (
 	"github.com/weaveworks/ignite/pkg/providers"
 	clientprovider "github.com/weaveworks/ignite/pkg/providers/client"
 	cniprovider "github.com/weaveworks/ignite/pkg/providers/cni"
+
+	//containerdprovider "github.com/weaveworks/ignite/pkg/providers/containerd"
 	dockerprovider "github.com/weaveworks/ignite/pkg/providers/docker"
 	storageprovider "github.com/weaveworks/ignite/pkg/providers/storage"
 )
@@ -11,7 +13,8 @@ import (
 // NOTE: Provider initialization is order-dependent!
 // E.g. the network plugin depends on the runtime.
 var Providers = []providers.ProviderInitFunc{
-	dockerprovider.SetDockerRuntime,   // Use the Docker runtime
+	dockerprovider.SetDockerRuntime, // Use the Docker runtime
+	//containerdprovider.SetContainerdRuntime, // Use the containerd runtime
 	dockerprovider.SetDockerNetwork,   // Use the Docker bridge network
 	cniprovider.SetCNINetworkPlugin,   // Use the CNI Network plugin
 	storageprovider.SetGenericStorage, // Use a generic storage implementation backed by a cache
--- pkg/providers/ignited/providers.go
+++ pkg/providers/ignited/providers.go
@@ -4,6 +4,8 @@ import (
 	"github.com/weaveworks/ignite/pkg/providers"
 	clientprovider "github.com/weaveworks/ignite/pkg/providers/client"
 	cniprovider "github.com/weaveworks/ignite/pkg/providers/cni"
+
+	//containerdprovider "github.com/weaveworks/ignite/pkg/providers/containerd"
 	dockerprovider "github.com/weaveworks/ignite/pkg/providers/docker"
 	manifeststorageprovider "github.com/weaveworks/ignite/pkg/providers/manifeststorage"
 )
@@ -11,7 +13,8 @@ import (
 // NOTE: Provider initialization is order-dependent!
 // E.g. the network plugin depends on the runtime.
 var Providers = []providers.ProviderInitFunc{
-	dockerprovider.SetDockerRuntime,            // Use the Docker runtime
+	dockerprovider.SetDockerRuntime, // Use the Docker runtime
+	//containerdprovider.SetContainerdRuntime,  // Use the containerd runtime
 	dockerprovider.SetDockerNetwork,            // Use the Docker bridge network
 	cniprovider.SetCNINetworkPlugin,            // Use the CNI Network plugin
 	manifeststorageprovider.SetManifestStorage, // Use the ManifestStorage implementation, backed by a cache
