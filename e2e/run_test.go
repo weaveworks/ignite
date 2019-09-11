878e5fbc01d90db16742fb58c914a992ff3fe992
leigh capili <leigh@null.net>
2019-09-11 06:39:40 -0600

    Refactor e2e and add docker+cni

--- e2e/run_test.go
+++ e2e/run_test.go
@@ -19,56 +19,70 @@ import (
 	"gotest.tools/assert"
 )
 
-var e2eHome = os.Getenv("IGNITE_E2E_HOME")
+var (
+	e2eHome   = os.Getenv("IGNITE_E2E_HOME")
+	igniteBin = path.Join(e2eHome, "bin/ignite")
+)
 
-func TestIgniteRunWithDockerAndDockerBridge(t *testing.T) {
-	// vmName should be unique for each test
-	const vmName = "e2e_test_ignite_run_docker_and_docker_bridge"
-
-	assert.Assert(t, e2eHome != "", "IGNITE_E2E_HOME should be set")
-
-	binary := path.Join(e2eHome, "bin/ignite")
-	cmd := exec.Command(binary,
-		"--runtime=docker",
-		"--network-plugin=docker-bridge",
-		"run", "--name="+vmName,
-		"weaveworks/ignite-ubuntu")
+// stdCmd builds an *exec.Cmd hooked up to Stdout/Stderr by default
+func stdCmd(name string, arg ...string) *exec.Cmd {
+	cmd := exec.Command(name, arg...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	return cmd
+}
+
+// runWithRuntimeAndNetworkPlugin is a helper for running a vm then forcing removal
+// vmName should be unique for each test
+func runWithRuntimeAndNetworkPlugin(t *testing.T, vmName, runtime, networkPlugin string) {
+	assert.Assert(t, e2eHome != "", "IGNITE_E2E_HOME should be set")
+
+	runCmd := stdCmd(
+		igniteBin,
+		"--runtime="+runtime,
+		"--network-plugin="+networkPlugin,
+		"run", "--name="+vmName,
+		"weaveworks/ignite-ubuntu",
+	)
+	runErr := runCmd.Run()
 
 	defer func() {
-		cmd := exec.Command(binary,
-			"--runtime=docker",
-			"--network-plugin=docker-bridge",
-			"rm", "-f", vmName)
-		assert.Check(t, cmd.Run(), "vm removal should not fail")
+		rmvCmd := stdCmd(
+			igniteBin,
+			"--runtime="+runtime,
+			"--network-plugin="+networkPlugin,
+			"rm", "-f", vmName,
+		)
+		rmvErr := rmvCmd.Run()
+		assert.Check(t, rmvErr, fmt.Sprintf("vm removal should not fail: %q", rmvCmd.Args))
 	}()
 
-	assert.Check(t, err, fmt.Sprintf("%q should not fail to run", cmd.Args))
+	assert.Check(t, runErr, fmt.Sprintf("%q should not fail to run", runCmd.Args))
+}
+
+func TestIgniteRunWithDockerAndDockerBridge(t *testing.T) {
+	runWithRuntimeAndNetworkPlugin(
+		t,
+		"e2e_test_ignite_run_docker_and_docker_bridge",
+		"docker",
+		"docker-bridge",
+	)
+}
+
+func TestIgniteRunWithDockerAndCNI(t *testing.T) {
+	runWithRuntimeAndNetworkPlugin(
+		t,
+		"e2e_test_ignite_run_docker_and_cni",
+		"docker",
+		"cni",
+	)
 }
 
 func TestIgniteRunWithContainerdAndCNI(t *testing.T) {
-	// vmName should be unique for each test
-	const vmName = "e2e_test_ignite_run_containerd_and_cni"
-
-	binary := path.Join(e2eHome, "bin/ignite")
-	cmd := exec.Command(binary,
-		"--runtime=containerd",
-		"--network-plugin=cni",
-		"run", "--name="+vmName,
-		"weaveworks/ignite-ubuntu")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-
-	defer func() {
-		cmd := exec.Command(binary,
-			"--runtime=containerd",
-			"--network-plugin=cni",
-			"rm", "-f", vmName)
-		assert.Check(t, cmd.Run(), "vm removal should not fail")
-	}()
-
-	assert.Check(t, err, fmt.Sprintf("%q should not fail to run", cmd.Args))
+	runWithRuntimeAndNetworkPlugin(
+		t,
+		"e2e_test_ignite_run_containerd_and_cni",
+		"containerd",
+		"cni",
+	)
 }
