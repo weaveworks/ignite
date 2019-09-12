5370463957b3b21934c3132ec3b5dbfd55e85d04
Chanwit Kaewkasi <chanwit@gmail.com>
2019-09-12 23:13:02 +0700

    add wait for SSH server properly started when --ssh is used

--- cmd/ignite/run/start.go
+++ cmd/ignite/run/start.go
@@ -2,6 +2,8 @@ package run
 
 import (
 	"fmt"
+	"net"
+	"time"
 
 	"github.com/weaveworks/ignite/pkg/operations"
 	"github.com/weaveworks/ignite/pkg/preflight/checkers"
@@ -45,6 +47,30 @@ func Start(so *startOptions) error {
 
 	if err := operations.StartVM(so.vm, so.Debug); err != nil {
 		return err
+	}
+
+	// When --ssh is enabled, then wait until SSH service started on port 22
+	ssh := so.vm.Spec.SSH
+	if ssh != nil && ssh.Generate {
+		if len(so.vm.Status.IPAddresses) > 0 {
+			addr := so.vm.Status.IPAddresses[0].String() + ":22"
+			var err error
+			for i := 0; i < 500; i++ {
+				conn, dialErr := net.DialTimeout("tcp", addr, 100*time.Millisecond)
+				if conn != nil {
+					defer conn.Close()
+					err = nil
+					break
+				}
+				err = dialErr
+				time.Sleep(100 * time.Millisecond)
+			}
+			if err != nil {
+				if err, ok := err.(*net.OpError); ok && err.Timeout() {
+					return fmt.Errorf("Tried connecting to SSH but timed out %s", err)
+				}
+			}
+		}
 	}
 
 	// If starting interactively, attach after starting
