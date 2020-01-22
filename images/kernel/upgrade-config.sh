8c34cc54380b50e270f570898b14c140fa45670f
Lucas Käldström <lucas@weave.works>
2020-01-22 23:29:11 +0200

    Add arm64 support to the kernel build process

--- images/kernel/upgrade-config.sh
+++ images/kernel/upgrade-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KERNEL_BUILDER_IMAGE=luxas/kernel-builder:gcc-7-2
+KERNEL_BUILDER_IMAGE=luxas/kernel-builder:gcc-7
 
 if [[ $# != 2 ]]; then
     echo "Usage: $0 [FROM] [TO]"
@@ -10,12 +10,19 @@ fi
 FROM=$1
 TO=$2
 VERSION=$(echo ${TO} | rev | cut -d- -f1 | rev)
+ARCH=$(echo ${TO} | cut -d- -f2)
+
+# Only set the extra flag for non-amd64 arches
+if [[ ${ARCH} != amd64 ]]; then
+    ARCH_PARAMETER="-e ARCH=${ARCH}"
+fi
 
 if [[ ${FROM} != ${TO} ]]; then
     cp ${FROM} ${TO}
 fi
 
 docker run -it \
+    ${ARCH_PARAMETER} \
 	-v $(pwd)/${TO}:/tmp/.config \
     ${KERNEL_BUILDER_IMAGE} /bin/bash -c "\
         git checkout v${VERSION} && \
