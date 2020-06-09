47688985d84a1f7bde4e12779dd284233ee48872
Lucas Käldström <lucas@weave.works>
2020-06-09 14:51:43 +0300

    Leverage the new gitdir package in libgitops
    (cherry picked from commit d2da42031950f2b683aaaa23cbfac681041ea794)

--- cmd/ignited/cmd/gitops.go
+++ cmd/ignited/cmd/gitops.go
@@ -2,24 +2,43 @@ package cmd
 
 import (
 	"io"
+	"io/ioutil"
+	"time"
 
 	"github.com/lithammer/dedent"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/weaveworks/ignite/pkg/gitops"
 	"github.com/weaveworks/ignite/pkg/util"
+	"github.com/weaveworks/libgitops/pkg/gitdir"
 )
 
 type gitOpsFlags struct {
-	branch string
-	paths  []string
+	branch   string
+	interval time.Duration
+	timeout  time.Duration
+
+	identityFile string
+	username     string
+	password     string
+
+	paths     []string
+	hostsFile string
 }
 
 // NewCmdGitOps runs the GitOps functionality of Ignite
 func NewCmdGitOps(out io.Writer) *cobra.Command {
 	f := &gitOpsFlags{
-		branch: "master",
-		paths:  []string{},
+		branch:   "master",
+		interval: 30 * time.Second,
+		timeout:  1 * time.Minute,
+
+		identityFile: "",
+		username:     "",
+		password:     "",
+
+		//paths:        []string{},
+		//hostsFile:    "~/.ssh/known_hosts",
 	}
 	cmd := &cobra.Command{
 		Use:   "gitops <repo-url>",
@@ -35,7 +54,24 @@ func NewCmdGitOps(out io.Writer) *cobra.Command {
 		`),
 		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			util.GenericCheckErr(gitops.RunGitOps(args[0], f.branch, f.paths))
+			opts := gitdir.GitDirectoryOptions{
+				Branch:   f.branch,
+				Interval: f.interval,
+				Timeout:  f.timeout,
+			}
+			if f.identityFile != "" {
+				var err error
+				opts.IdentityFileContent, err = ioutil.ReadFile(f.identityFile)
+				util.GenericCheckErr(err)
+			}
+			if f.username != "" {
+				opts.Username = &f.username
+			}
+			if f.password != "" {
+				opts.Password = &f.password
+			}
+
+			util.GenericCheckErr(gitops.RunGitOps(args[0], opts))
 		},
 	}
 
@@ -45,6 +81,19 @@ func NewCmdGitOps(out io.Writer) *cobra.Command {
 
 func addGitOpsFlags(fs *pflag.FlagSet, f *gitOpsFlags) {
 	fs.StringVarP(&f.branch, "branch", "b", f.branch, "What branch to sync")
-	fs.StringSliceVarP(&f.paths, "paths", "p", f.paths, "What subdirectories to care about. Default the whole repository")
-	// TODO: Add ssh key opts etc.
+	fs.DurationVar(&f.interval, "interval", f.interval, "Sync interval for pushing to and pulling from the remote")
+	fs.DurationVar(&f.timeout, "timeout", f.timeout, "Git operation (clone, push, pull) timeout")
+
+	fs.StringVar(&f.identityFile, "identity-file", f.identityFile, "What SSH identity file to use for pushing")
+	fs.StringVar(&f.username, "https-username", f.username, "What username to use when authenticating with Git over HTTPS")
+	fs.StringVar(&f.password, "https-password", f.password, "What password/access token to use when authenticating with Git over HTTPS")
+
+	// TODO: We need to add path prefix support to the WatchStorage to support this
+	// fs.StringSliceVarP(&f.paths, "paths", "p", f.paths, "What subdirectories to care about. Default the whole repository")
+
+	// TODO: When https://github.com/fluxcd/toolkit/issues/2 is fixed and the
+	// https://github.com/fluxcd/source-controller/tree/master/internal/crypto/ssh/knownhosts package
+	// can be vendored, we'll add support for reading a hosts file. In the meantime, the
+	// SSH_KNOWN_HOSTS variable can be used.
+	// fs.StringVar(&f.hostsFile, "hosts-file", f.hostsFile, "What hosts file to use")
 }
