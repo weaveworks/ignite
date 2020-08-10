e8b05d86c13d17df35295665791cbccc69cea92c
leigh capili <leigh@null.net>
2020-08-10 10:26:54 -0600

    Skip TestVolumeWithDockerAndDockerBridge #658

--- e2e/run_volume_test.go
+++ e2e/run_volume_test.go
@@ -172,6 +172,8 @@ func runVolume(t *testing.T, vmName, runtime, networkPlugin string) {
 }
 
 func TestVolumeWithDockerAndDockerBridge(t *testing.T) {
+	// TODO: https://github.com/weaveworks/ignite/issues/658
+	t.Skip("SKIPPING\nThis test fails to stop the VM within docker\nTODO: https://github.com/weaveworks/ignite/issues/658")
 	runVolume(
 		t,
 		"e2e_test_volume_docker_and_docker_bridge",
