e5df632a652ac8e7f3bccefe522ec4245fedcb4e
Sunny <darkowlzz@protonmail.com>
2020-09-21 18:17:43 +0530

    Add tools.go and vendor code-gen tools
    This is required to be able to update dependencies without usind docker
    in github actions.
    Vendoring the code-generation tools and running them with go run helps
    avoid running go get to install the tools and updating the mod file
    with unused dependencies.

---
+++ tools.go
@@ -0,0 +1,10 @@
+// +build tools
+
+package tools
+
+import (
+	_ "k8s.io/code-generator/cmd/conversion-gen"
+	_ "k8s.io/code-generator/cmd/deepcopy-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+)
