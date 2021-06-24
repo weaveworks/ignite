668b5fe42a4dfd11ab3908540dd6349f9376b7c6
networkop <mmkashin@gmail.com>
2021-06-24 15:07:25 +0100

    Removing waitForSSH from exec

--- cmd/ignite/run/exec.go
+++ cmd/ignite/run/exec.go
@@ -2,7 +2,6 @@ package run
 
 import (
 	api "github.com/weaveworks/ignite/pkg/apis/ignite"
-	"github.com/weaveworks/ignite/pkg/constants"
 )
 
 // ExecFlags contains the flags supported by the exec command.
@@ -31,8 +30,5 @@ func (ef *ExecFlags) NewExecOptions(vmMatch string, command ...string) (eo *Exec
 
 // Exec executes command in a VM based on the provided ExecOptions.
 func Exec(eo *ExecOptions) error {
-	if err := waitForSSH(eo.vm, constants.SSH_DEFAULT_TIMEOUT_SECONDS, 5); err != nil {
-		return err
-	}
 	return runSSH(eo.vm, eo.IdentityFile, eo.command, eo.Tty, eo.Timeout)
 }
